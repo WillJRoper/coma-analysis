ed1d6940de7155b08c26b606dbf2f1c6a4416109
wjr21 <wjr21@sussex.ac.uk>
2023-06-26 14:07:27 +0100

    Add imaging file

---
+++ img_zoom_region.py
@@ -0,0 +1,46 @@
+import sys
+import h5py
+import unyt
+from synthesizer.imaging.images import ParticleImage
+import matplotlib.colors as cm
+import matplotlib.pyplot as plt
+import swiftascmaps
+
+
+# Get snapshot
+snap = sys.argv[1]
+
+# Get data
+hdf = h5py.File("snapshots/COMA_N155_" + snap + ".hdf5", "r")
+hdf["Header"].attrs["Redshift"]
+pos = hdf["PartType1"]["Coordinates"][:]
+mass = hdf["PartType1"]["Masses"][:] * 10 ** 10
+hdf.close()
+
+print("Got data...")
+
+# Get image
+img = ParticleImage(
+    0.00233195 * unyt.Mpc * 2,
+    fov=np.max((np.max(pos[:, 0]) - np.min(pos[:, 0]),
+                np.max(pos[:, 1]) - np.min(pos[:, 1]),
+                np.max(pos[:, 2]) - np.min(pos[:, 2]))) * unyt.Mpc,
+    positions=pos * unyt.Mpc,
+    smoothing_lengths=np.full(pos.shape[0], 0.00233195) * unyt.Mpc,
+    pixel_values=mass
+)
+img.get_hist_img()
+
+print("Got image...")
+ 
+# Plot image
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(
+    img.img,
+    extent=(pos[:, 0].min(), pos[:, 0].max(), pos[:, 1].min(), pos[:, 1].max()),
+    norm=cm.LogNorm(),
+    cmap="swift.nineteen_eighty_nine")
+fig.savefig("plots/COMA_low_res_test_xy_" + snap + ".png", dpi=300,
+            bbox_inches="tight")
+plt.close(fig)
