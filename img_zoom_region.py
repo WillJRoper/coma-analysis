eb851922418c3460a853652fa774fb97199d22c8
wjr21 <wjr21@sussex.ac.uk>
2023-06-30 14:28:24 +0100

    Changing loop range

--- img_zoom_region.py
+++ img_zoom_region.py
@@ -11,7 +11,7 @@ import swiftascmaps
 # Get snapshot
 if len(sys.argv) == 1:
 
-    snaps = [str(i).zfill(4) for i in range(22, 24)]
+    snaps = [str(i).zfill(4) for i in range(24, 28)]
 
     for snap in snaps:
 
