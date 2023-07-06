f7283341e3a3bf9ce97978efd8f4fc285bda65c5
wjr21 <wjr21@sussex.ac.uk>
2023-07-06 14:53:07 +0100

    Adding age and distance to image

--- img_zoom_region.py
+++ img_zoom_region.py
@@ -12,7 +12,7 @@ def get_image(snap):
 
     # Get data
     hdf = h5py.File("../low_res/snapshots/COMA_N155_" + snap + ".hdf5", "r")
-    hdf["Header"].attrs["Redshift"]
+    redshift = hdf["Header"].attrs["Redshift"]
     pos = hdf["PartType1"]["Coordinates"][:]
     mass = hdf["PartType1"]["Masses"][:] * 10 ** 10
     hdf.close()
@@ -40,16 +40,49 @@ def get_image(snap):
     # Plot image
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.axis("off")
     ax.imshow(
         img.img,
         extent=(pos[:, 0].min(), pos[:, 0].max(),
                 pos[:, 1].min(), pos[:, 1].max()),
         norm=cm.LogNorm(vmin=mass[0] * 0.5, vmax=None, clip=True),
         cmap="swift.nineteen_eighty_nine")
+
+    ax.tick_params(axis='both', left=False, top=False, right=False,
+                   bottom=False, labelleft=False,
+                   labeltop=False, labelright=False,
+                   labelbottom=False)
+
+    ax.text(0.975, 0.05, "$t=$%.1f Gyr" % cosmo.age(redshift).value,
+            transform=ax.transAxes, verticalalignment="top",
+            horizontalalignment='right', fontsize=1, color="w")
+
+    ax.plot([0.05, 0.15], [0.025, 0.025], lw=0.1, color='w',
+            clip_on=False,
+            transform=ax.transAxes)
+    
+    ax.plot([0.05, 0.05], [0.022, 0.027], lw=0.15, color='w',
+            clip_on=False,
+            transform=ax.transAxes)
+    ax.plot([0.15, 0.15], [0.022, 0.027], lw=0.15, color='w',
+            clip_on=False,
+            transform=ax.transAxes)
+    
+    axis_to_data = ax.transAxes + ax.transData.inverted()
+    left = axis_to_data.transform((0.05, 0.075))
+    right = axis_to_data.transform((0.15, 0.075))
+    dist = extent[1] * (right[0] - left[0]) / (
+        ang_extent[1] - ang_extent[0])
+
+    ax.text(0.1, 0.055, "%.2f cMpc" % dist,
+            transform=ax.transAxes, verticalalignment="top",
+            horizontalalignment='center', fontsize=1, color="w")
+    
+    plt.margins(0, 0)
+    
     fig.savefig("plots/COMA_low_res_test_xy_" + snap + ".png", dpi=300,
-                bbox_inches="tight")
+                bbox_inches="tight", pad_inches=0)
     plt.close(fig)
+      
 
 snaps = [str(i).zfill(4) for i in range(0, 35)]
 
